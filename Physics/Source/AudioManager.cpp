d2e193c5688620abee2a156264bbeaf936a714f9
Cheng-PC <tayhaocheng@yahoo.com>
2019-07-22 00:58:42 +0800

    add check for engine

--- Physics/Source/AudioManager.cpp
+++ Physics/Source/AudioManager.cpp
@@ -14,6 +14,8 @@ AudioManager::~AudioManager()
 
 void AudioManager::PlayBGM(std::string filePath)
 {
+	if (!engine)
+		return;
 	filePath = Resources::Path::Audio + filePath;
 	float fVolume = std::stof(Preferences::GetPref(Resources::PreferencesTerm::AudioVolume));
 	engine->setSoundVolume(fVolume);
@@ -22,13 +24,8 @@ void AudioManager::PlayBGM(std::string filePath)
 
 void AudioManager::Play3D(std::string filePath, Vector3 position)
 {
-	filePath = Resources::Path::Audio + filePath;
-	try
-	{
-		engine->play3D(filePath.c_str(), irrklang::vec3df(position.x, position.y, position.z), false, false, false, irrklang::ESM_AUTO_DETECT, true);
-	}
-	catch (const std::exception&)
-	{
+	if (!engine)
 		return;
-	}
+	filePath = Resources::Path::Audio + filePath;
+	engine->play3D(filePath.c_str(), irrklang::vec3df(position.x, position.y, position.z), false, false, false, irrklang::ESM_AUTO_DETECT, true);
 }
