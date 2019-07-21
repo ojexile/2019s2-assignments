0e94508c09a7862d0bf522e6df2c86f829f7adc7
Cheng-PC <tayhaocheng@yahoo.com>
2019-07-21 19:37:16 +0800

    remove animation quad

--- Physics/Source/MeshBuilder.cpp
+++ Physics/Source/MeshBuilder.cpp
@@ -6,7 +6,6 @@
 #include "LoadOBJ.h"
 #include "Resources.h"
 
-Mesh* MeshBuilder::m_AnimatedQuad = nullptr;
 /******************************************************************************/
 /*!
 \brief
@@ -604,66 +603,58 @@ Mesh* MeshBuilder::GenerateSkyPlane(const std::string &meshName, Color color, in
 }
 AnimatedMesh* MeshBuilder::GenerateAnimatedMesh(std::string sMeshName, int numRow, int numCol, int start, int end, float time, bool loop)
 {
-	if (!m_AnimatedQuad)
+	Vertex v;
+	std::vector<Vertex> vertex_buffer_data;
+	std::vector<GLuint> index_buffer_data;
+
+	float width = 1.f / numCol;
+	float height = 1.f / numRow;
+	int offset = 0;
+
+	for (int i = 0; i < numRow; ++i)
 	{
-		Vertex v;
-		std::vector<Vertex> vertex_buffer_data;
-		std::vector<GLuint> index_buffer_data;
-
-		float width = 1.f / numCol;
-		float height = 1.f / numRow;
-		int offset = 0;
-
-		for (int i = 0; i < numRow; ++i)
+		for (int j = 0; j < numCol; ++j)
 		{
-			for (int j = 0; j < numCol; ++j)
-			{
-				float u1 = j * width;
-				float v1 = 1.f - height - i * height;
+			float u1 = j * width;
+			float v1 = 1.f - height - i * height;
 
-				v.pos = { -0.5f, -0.5f, 0.0f }; //top left
-				v.texCoord.Set(u1, v1);
-				vertex_buffer_data.push_back(v);
+			v.pos = { -0.5f, -0.5f, 0.0f }; //top left
+			v.texCoord.Set(u1, v1);
+			vertex_buffer_data.push_back(v);
 
-				v.pos = { 0.5f, -0.5f, 0.0f }; //top right
-				v.texCoord.Set(u1 + width, v1);
-				vertex_buffer_data.push_back(v);
+			v.pos = { 0.5f, -0.5f, 0.0f }; //top right
+			v.texCoord.Set(u1 + width, v1);
+			vertex_buffer_data.push_back(v);
 
-				v.pos = { 0.5f, 0.5f, 0.0f }; //bottom left
-				v.texCoord.Set(u1 + width, v1 + height);
-				vertex_buffer_data.push_back(v);
+			v.pos = { 0.5f, 0.5f, 0.0f }; //bottom left
+			v.texCoord.Set(u1 + width, v1 + height);
+			vertex_buffer_data.push_back(v);
 
-				v.pos = { -0.5f, 0.5f, 0.0f }; //bottom right
-				v.texCoord.Set(u1, v1 + height);
-				vertex_buffer_data.push_back(v);
+			v.pos = { -0.5f, 0.5f, 0.0f }; //bottom right
+			v.texCoord.Set(u1, v1 + height);
+			vertex_buffer_data.push_back(v);
 
-				index_buffer_data.push_back(offset + 0);
-				index_buffer_data.push_back(offset + 1);
-				index_buffer_data.push_back(offset + 2);
-				index_buffer_data.push_back(offset + 0);
-				index_buffer_data.push_back(offset + 2);
-				index_buffer_data.push_back(offset + 3);
-				offset += 4;
-			}
+			index_buffer_data.push_back(offset + 0);
+			index_buffer_data.push_back(offset + 1);
+			index_buffer_data.push_back(offset + 2);
+			index_buffer_data.push_back(offset + 0);
+			index_buffer_data.push_back(offset + 2);
+			index_buffer_data.push_back(offset + 3);
+			offset += 4;
 		}
-
-		m_AnimatedQuad = new Mesh(sMeshName);
-
-		m_AnimatedQuad->mode = Mesh::DRAW_TRIANGLES;
-
-		glBindBuffer(GL_ARRAY_BUFFER, m_AnimatedQuad->vertexBuffer);
-		glBufferData(GL_ARRAY_BUFFER, vertex_buffer_data.size() * sizeof(Vertex), &vertex_buffer_data[0], GL_STATIC_DRAW);
-
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_AnimatedQuad->indexBuffer);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, index_buffer_data.size() * sizeof(GLuint), &index_buffer_data[0], GL_STATIC_DRAW);
-
-		m_AnimatedQuad->indexSize = index_buffer_data.size();
 	}
-	AnimatedMesh *anim = new AnimatedMesh(sMeshName, numRow, numCol, start, end, time, loop, m_AnimatedQuad);
-	return anim;
-}
 
-void MeshBuilder::Release()
-{
-	delete m_AnimatedQuad;
+	Mesh* mesh = new Mesh(sMeshName);
+
+	mesh->mode = Mesh::DRAW_TRIANGLES;
+
+	glBindBuffer(GL_ARRAY_BUFFER, mesh->vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, vertex_buffer_data.size() * sizeof(Vertex), &vertex_buffer_data[0], GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh->indexBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, index_buffer_data.size() * sizeof(GLuint), &index_buffer_data[0], GL_STATIC_DRAW);
+
+	mesh->indexSize = index_buffer_data.size();
+	AnimatedMesh *anim = new AnimatedMesh(sMeshName, numRow, numCol, start, end, time, loop, mesh);
+	return anim;
 }
