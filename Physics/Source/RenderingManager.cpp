2eb135a137b4b9cd24ecb420b13de4b03f5f6e44
Cheng-PC <tayhaocheng@yahoo.com>
2019-07-22 00:39:54 +0800

    fix flare

--- Physics/Source/RenderingManager.cpp
+++ Physics/Source/RenderingManager.cpp
@@ -205,9 +205,9 @@ void RenderingManager::RenderWorld(Scene* scene)
 	Vector3 CamToLight = LightPos - vCamPos;
 	float angleBetweenRad = acos(CamToLight.Dot(vCamDir) / (vCamDir.Length() * CamToLight.Length()));
 	float angleBetweenDeg = Math::RadianToDegree(angleBetweenRad);
-	this->m_fFlareVal = 180 - angleBetweenDeg - 120;
-	m_fFlareVal /= 360;
-	m_fFlareVal = Math::Clamp(m_fFlareVal, 0.f, .6f);
+	this->m_fFlareVal = 45 - angleBetweenDeg;
+	m_fFlareVal /= 70;
+	m_fFlareVal = Math::Clamp(m_fFlareVal, 0.f, .85f);
 
 	GameObjectManager* GOM = scene->GetGameObjectManager();
 	std::map<std::string, LayerData*>::iterator it;
