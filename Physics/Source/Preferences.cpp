6cf6cea3ef13080b974838702192fbf56fda3c5a
Chindianese-Laptop <tayha@DESKTOP-DECJO7B>
2019-08-12 14:38:47 +0800

    set default logger update rate

--- Physics/Source/Preferences.cpp
+++ Physics/Source/Preferences.cpp
@@ -23,7 +23,7 @@ void Preferences::InitDefault()
 	m_map_Data[Resources::PreferencesTerm::ConsolePosition] = "20,740";
 	m_map_Data[Resources::PreferencesTerm::FontSize] = "12,22";
 	m_map_Data[Resources::PreferencesTerm::AudioVolume] = "0.1";
-	m_map_Data[Resources::PreferencesTerm::LogUpdateRate] = "10";
+	m_map_Data[Resources::PreferencesTerm::LogUpdateRate] = "1";
 	m_map_Data[Resources::PreferencesTerm::LogUser] = "ALL";
 
 	GetData();
