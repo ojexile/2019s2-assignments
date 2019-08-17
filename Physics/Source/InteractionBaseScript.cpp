8f416530c28e9719c47b76cda126b5e00ffb9c53
Lorenzo Yanga <lorenzoyanga@gmail.com>
2019-08-17 14:28:47 +0800

    interaction base script (abstract)

---
+++ Physics/Source/InteractionBaseScript.cpp
@@ -0,0 +1,9 @@
+#include "InteractionBaseScript.h"
+
+InteractionBaseScript::InteractionBaseScript()
+	: m_bIsTriggered(false),
+	m_fTimeElapsed(0.f),
+	m_fMaximumTimeElapsed(-1.f)
+{
+
+}
---
+++ Physics/Source/InteractionBaseScript.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "ScriptComponent.h"
+
+/*
+author: lorenzo yanga
+desc: interaction base class. abstract.
+	  needs to be activated with the Trigger() function first
+	  before it can do anything in the update loop
+
+*/
+
+class InteractionBaseScript : public ScriptComponent
+{
+private:
+	bool m_bIsTriggered;
+	float m_fTimeElapsed; // counting from trigger
+	float m_fMaximumTimeElapsed; // counting from trigger
+public:
+	InteractionBaseScript();
+	virtual ~InteractionBaseScript();
+
+	virtual void Update(double dt) = 0;
+	virtual void Trigger(GameObject* go1, GameObject * go2) = 0;
+	virtual ComponentBase* Clone() = 0;
+};
