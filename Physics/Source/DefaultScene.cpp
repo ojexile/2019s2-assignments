7281b0b425d5f6a2b7fd53b6d30e67432e8574d2
Nam Kai Zhe <180716S@mymail.nyp.edu.sg>
2019-08-19 09:29:58 +0800

    re-added audio

--- Physics/Source/DefaultScene.cpp
+++ Physics/Source/DefaultScene.cpp
@@ -179,4 +179,12 @@ void DefaultScene::Init()
 	go->AddComponent(new RenderComponent(dataContainer->GetMesh("Text"), "oof", false));
 	go->RENDER->Set3DBillboard(true);
 	go->RENDER->SetColor(0, 1, 1);
+
+	AudioManager::GetInstance()->PlayBGM("bgm_01.ogg", "low_synth");
+	AudioManager::GetInstance()->SetBGMVolume(0, "low_synth");
+	AudioManager::GetInstance()->QueueFade(1, 0.05, "low_synth");
+	AudioManager::GetInstance()->PlayBGM("bgm_02.ogg", "low_pad");
+	AudioManager::GetInstance()->PlayBGM("bgm_03.ogg", "low_piano");
+	AudioManager::GetInstance()->PlayBGM("bgm_04.ogg", "high_piano");
+	AudioManager::GetInstance()->SetBGMVolume(0, "high_piano");
 }
