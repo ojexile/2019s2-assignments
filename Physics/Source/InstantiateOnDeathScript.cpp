ad1c1edb8579d755cdac69b51dbc47eeafa15de0
Lorenzo Yanga <lorenzoyanga@gmail.com>
2019-08-19 18:00:01 +0800

    instantiate on death script

---
+++ Physics/Source/InstantiateOnDeathScript.cpp
@@ -0,0 +1,23 @@
+#include "InstantiateOnDeathScript.h"
+
+InstantiateOnDeathScript::InstantiateOnDeathScript()
+	: m_bHasTriggered(false)
+{
+}
+
+InstantiateOnDeathScript::~InstantiateOnDeathScript()
+{
+}
+
+GameObject * InstantiateOnDeathScript::AddGOToList(GameObject * go)
+{
+	return nullptr;
+}
+
+void InstantiateOnDeathScript::Update(double dt)
+{
+}
+
+void InstantiateOnDeathScript::Collide(GameObject * go)
+{
+}
