bb4e5a2683229c160c122a44a4faae4f6b03f87a
Lorenzo Yanga <lorenzoyanga@gmail.com>
2019-08-20 15:12:15 +0800

    dsp trigger

--- Physics/Source/DebrisSpawningScript.cpp
+++ Physics/Source/DebrisSpawningScript.cpp
@@ -33,13 +33,17 @@ void DebrisSpawningScript::SetMinMax(int min, int max)
 void DebrisSpawningScript::Trigger()
 {
 	int spawncount = Math::RandIntMinMax(m_iMin, m_iMax);
-
+	
+	LZ_LOG("rokspawned");
 	for (int i = 0; i <= spawncount; i++)
 	{
 		auto go = Instantiate(DataContainer::GetInstance()->GetGameObject(m_sDebrisName));
 		go->RIGID->AddForce(Vector3(Math::RandFloatMinMax(-1000, 1000), Math::RandFloatMinMax(100, 1000), Math::RandFloatMinMax(-1000, 1000)));
-		go->TRANS->SetPosition(TRANS->GetPosition());
+		Vector3 pos = this->TRANS->GetPosition();
+		go->TRANS->SetPosition(pos);
 	}
+
+	DestroySelf();
 }
 
 
