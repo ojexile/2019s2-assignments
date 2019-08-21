83cae1f210d6170442fab00233e2cbcec9d34948
Lorenzo Yanga <lorenzoyanga@gmail.com>
2019-08-21 10:16:16 +0800

    spawn rate based on how many times you want to cycle per second

--- Physics/Source/AdvancedParticleSpawnerScript.cpp
+++ Physics/Source/AdvancedParticleSpawnerScript.cpp
@@ -23,86 +23,93 @@ void AdvancedParticleSpawnerScript::Update(double dt)
 
 	if (m_fCurrentTime <= m_fSpawnRate)
 		return;
-
-	GameObject* go = Instantiate(m_ParticleRef, m_sLayer);
-	float scalar = go->RIGID->GetVel().Length();
-	if (scalar < Math::EPSILON)
-		scalar = 1.f;
-	float q1x, q1z;
-
-	if (m_spawnerType == SPEW)
+	
+	do
 	{
-		q1x = Math::RandFloatMinMax(-1.f, 1.f);
-		q1z = Math::RandFloatMinMax(-1.f, 1.f);
+		GameObject* go = Instantiate(m_ParticleRef, m_sLayer);
+		float scalar = go->RIGID->GetVel().Length();
+		if (scalar < Math::EPSILON)
+			scalar = 1.f;
+		float q1x, q1z;
 
-		go->RIGID->AddForce(scalar * Vector3(q1x, 0, q1z));
-	}
-
-	else if (m_spawnerType == CIRCULAR)
-	{
-		float spaceperdegree = 360.f / (float)m_iSpawnCount;
-
-		for (float i = 0.f; i < 360.f; i += spaceperdegree)
+		if (m_spawnerType == SPEW)
 		{
-			float scalar = go->RIGID->GetVel().Length();
-			
-			if ((int)i >= 270)
+			for (int i = 0; i <= m_iSpawnCount; i++)
 			{
-				if ((int)i == 270)
-				{
-					go->RIGID->SetVel(scalar * Vector3(0, 0, -1));
-					go->TRANS->SetPosition(TRANS->GetPosition());
-				}
-				q1z = tan(i);
-				q1x = sqrt(1.f - q1z * q1z);
-				Vector3 vector = Vector3(q1x, 0, -q1z);
-				go->RIGID->AddForce(scalar * vector);
-				go->TRANS->SetPosition(TRANS->GetPosition());
-			}
-			else if ((int)i >= 180)
-			{
-				if ((int)i == 180)
-				{
-					go->RIGID->SetVel(scalar * Vector3(-1, 0, 0));
-					go->TRANS->SetPosition(TRANS->GetPosition());
-				}
-				q1z = tan(i);
-				q1x = sqrt(1.f - q1z * q1z);
-				Vector3 vector = Vector3(-q1x, 0, -q1z);
-				go->RIGID->AddForce(scalar * vector);
-				go->TRANS->SetPosition(TRANS->GetPosition());
-			}
-			else if ((int)i >= 90)
-			{
-				if ((int)i == 90)
-				{
-					go->RIGID->SetVel(scalar * Vector3(0, 0, 1));
-					go->TRANS->SetPosition(TRANS->GetPosition());
-				}
-				q1z = sin(i);
-				q1x = sqrt(1.f - q1z * q1z);
-				Vector3 vector = Vector3(-q1x, 0, q1z);
-				go->RIGID->AddForce(scalar * vector);
-				go->TRANS->SetPosition(TRANS->GetPosition());
-			}
-			else if ((int)i >= 0)
-			{
-				if ((int)i == 0)
-				{
-					go->RIGID->SetVel(scalar * Vector3(1, 0, 0));
-					go->TRANS->SetPosition(TRANS->GetPosition());
-				}
-				q1z = sin(i);
-				q1x = sqrt(1.f - q1z * q1z);
-				Vector3 vector = Vector3(q1x, 0, q1z);
-				go->RIGID->AddForce(scalar * vector);
-				go->TRANS->SetPosition(TRANS->GetPosition());
+				q1x = Math::RandFloatMinMax(-1.f, 1.f);
+				q1z = Math::RandFloatMinMax(-1.f, 1.f);
+
+				go->RIGID->AddForce(scalar * Vector3(q1x, 0, q1z));
+
 			}
 		}
-	
-	
-	}
-}
+
+		else if (m_spawnerType == CIRCULAR)
+		{
+			float spaceperdegree = 360.f / (float)m_iSpawnCount;
+
+			for (float i = 0.f; i < 360.f; i += spaceperdegree)
+			{
+				float scalar = go->RIGID->GetVel().Length();
+
+				if ((int)i >= 270)
+				{
+					if ((int)i == 270)
+					{
+						go->RIGID->SetVel(scalar * Vector3(0, 0, -1));
+						go->TRANS->SetPosition(TRANS->GetPosition());
+					}
+					q1z = tan(i);
+					q1x = sqrt(1.f - q1z * q1z);
+					Vector3 vector = Vector3(q1x, 0, -q1z);
+					go->RIGID->AddForce(scalar * vector);
+					go->TRANS->SetPosition(TRANS->GetPosition());
+				}
+				else if ((int)i >= 180)
+				{
+					if ((int)i == 180)
+					{
+						go->RIGID->SetVel(scalar * Vector3(-1, 0, 0));
+						go->TRANS->SetPosition(TRANS->GetPosition());
+					}
+					q1z = tan(i);
+					q1x = sqrt(1.f - q1z * q1z);
+					Vector3 vector = Vector3(-q1x, 0, -q1z);
+					go->RIGID->AddForce(scalar * vector);
+					go->TRANS->SetPosition(TRANS->GetPosition());
+				}
+				else if ((int)i >= 90)
+				{
+					if ((int)i == 90)
+					{
+						go->RIGID->SetVel(scalar * Vector3(0, 0, 1));
+						go->TRANS->SetPosition(TRANS->GetPosition());
+					}
+					q1z = sin(i);
+					q1x = sqrt(1.f - q1z * q1z);
+					Vector3 vector = Vector3(-q1x, 0, q1z);
+					go->RIGID->AddForce(scalar * vector);
+					go->TRANS->SetPosition(TRANS->GetPosition());
+				}
+				else if ((int)i >= 0)
+				{
+					if ((int)i == 0)
+					{
+						go->RIGID->SetVel(scalar * Vector3(1, 0, 0));
+						go->TRANS->SetPosition(TRANS->GetPosition());
+					}
+					q1z = sin(i);
+					q1x = sqrt(1.f - q1z * q1z);
+					Vector3 vector = Vector3(q1x, 0, q1z);
+					go->RIGID->AddForce(scalar * vector);
+					go->TRANS->SetPosition(TRANS->GetPosition());
+				}
+			}
+			
+
+		}
+		m_fCurrentTime -= m_fSpawnRate;
+	} while (m_fCurrentTime >= m_fSpawnRate);
 
 void AdvancedParticleSpawnerScript::Trigger()
 {
