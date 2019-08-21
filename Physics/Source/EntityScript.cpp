59b5eaab37e96041b1ba3e96a170fe616293361a
Lorenzo Yanga <lorenzoyanga@gmail.com>
2019-08-21 14:19:34 +0800

    fuck

--- Physics/Source/EntityScript.cpp
+++ Physics/Source/EntityScript.cpp
@@ -164,7 +164,7 @@ void EntityScript::Jump()
 	{
 		rb->SetVel(Vector3(rb->GetVel().x, 40 / rb->GetMass(), rb->GetVel().z));
 		m_bCanJump = false;
-		if (GetComponent<PlayerScript>() == nullptr)
+		if (GetComponent<PlayerScript>() != nullptr)
 		{
 			Notify("Jump");
 		}
