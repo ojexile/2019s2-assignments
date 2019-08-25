e5ab8c8b73c6e2022b268bb49b387ecf6dfe2d77
Cheng-PC <tayhaocheng@yahoo.com>
2019-08-26 03:20:53 +0800

    fix fps counter

--- Physics/Source/Engine.cpp
+++ Physics/Source/Engine.cpp
@@ -15,7 +15,7 @@
 #include "DefaultScene.h"
 #include "MainMenu.h"
 Renderer* Engine::m_Renderer;
-#define TIMINGS false
+#define TIMINGS true
 
 Engine::Engine()
 {
@@ -119,13 +119,13 @@ void Engine::Update(double dt)
 	}
 	if (TIMINGS)
 		CHENG_LOG("Time to update: ", STOP_S);
-	//Update coll--------------------------------------------------------------------------------
 	s.Reset();
 	//Update coll-------------------------------------------------------------------------------
 	StopWatch sw(true);
 	m_CollisionManager.Update(CurrentScene->GetGameObjectManager(), (m_frameCount & 255));
 	if (TIMINGS)
 		CHENG_LOG("Time to check collision: ", STOP_S);
+	s.Reset();
 	sw.Stop();
 	KZ_LOG("[Collision_Time_2]", " CollisionManager.Update() took " + sw.GetSTime() + "s");
 	// Update Observers
@@ -133,7 +133,6 @@ void Engine::Update(double dt)
 	// Remove to be destroyed--------------------------------------------------------------------------------
 	GOM->DestroyQueued();
 	//--------------------------------------------------------------------------------
-	s.Reset();
 	m_Renderer->Update(dt);
 	m_Renderer->Render(CurrentScene);
 	if (TIMINGS)
@@ -152,7 +151,6 @@ void Engine::Update(double dt)
 		coord.Y = 0;
 		SetConsoleCursorPosition(handle, coord);
 		//
-		//std::clock_t start = std::clock(); // --------------------------------------------------------------------------------
 		// Print current logger user
 		std::string sLogUser = Preferences::GetPref(Resources::PreferencesTerm::LogUser);
 		Locator::eLoggerUsers eLoggerUser = StringToUser(sLogUser);
@@ -175,7 +173,6 @@ void Engine::Update(double dt)
 			break;
 		}
 		m_fLogUpdateTimer = 0;
-		//double duration = (std::clock() - start) / (double)CLOCKS_PER_SEC; // --------------------------------------------------------------------------------
 	}
 }
 void Engine::CheckGOForObserver(GameObject* go, std::vector<GameObject*>* GOList)
