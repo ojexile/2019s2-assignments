03728fd670ac376ab43d8d03c2c58533900ed86f
Chindianese-Laptop <tayha@DESKTOP-DECJO7B>
2019-08-27 17:43:59 +0800

    increase coin scale

--- Physics/Source/MainMenu.cpp
+++ Physics/Source/MainMenu.cpp
@@ -127,6 +127,16 @@ void MainMenu::Init()
 		TutorialText, TutorialButt, TutBox, Ability0, Ability1));
 	go->SetDisableDistance(-1);
 	//
+	go = m_GOM.AddGameObject("UI");
+	go->TRANS->SetPosition(1800, 900, 1);
+	go->TRANS->SetScale(120);
+	go->AddComponent(new RenderComponent(dataContainer->GetMesh("QuadCentered")));
+	go->RENDER->SetColor(1, 1, 0);
+	go = m_GOM.AddGameObject("UI");
+	go->TRANS->SetPosition(1800 + 100, 900, 1);
+	int NumCoins = PlayerData::GetInstance()->GetCoins();
+	go->TRANS->SetPosition(1800 + 100, 900, 40);
+	go->AddComponent(new RenderComponent(dataContainer->GetMesh("Text"), std::to_string(NumCoins)));
 	/// End UI================================================================================
 	/// Player================================================================================
 	//Gun------------------------------------------------------------------------------------
