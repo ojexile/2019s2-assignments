2d4aa5ecd602eb2af8570f8cef3d5fb777b3d6f8
lorenzo <lorenzoyanga@gmail.com>
2019-08-27 11:16:17 +0800

    lootbox will self-destruct when interacted with

--- Physics/Source/InteractableObCom.cpp
+++ Physics/Source/InteractableObCom.cpp
@@ -5,6 +5,9 @@
 #include "SpawnLootScript.h"
 #include "SceneManager.h"
 #include "DataContainer.h"
+#include "ParticleSpawnerScript.h"
+#include "AdvancedParticleSpawnerScript.h"
+#include "ScalePatternScript.h"
 
 InteractableObCom::InteractableObCom()
 {
@@ -26,13 +29,21 @@ void InteractableObCom::Notify(ComponentBase* com, std::string msg)
 	SpawnLootScript* lootscript = GetComponent<SpawnLootScript>();
 	if (lootscript)
 	{
+		if (lootscript->m_isActivated)
+			return;
+
+		// boolean to flip
+		lootscript->m_isActivated = true;
+		lootscript->GetGO()->AddComponent(new ScalePatternScript(ScalePatternScript::SHRINK, 1.f, 1.f));
+
 		auto dasd = DataContainer::GetInstance()->GetGameObject("treasureball");
 		GameObject* loot = Instantiate(dasd, Vector3(0, 0, 0));
 		float yoffset = (TRANS->GetScale() * 1.1f).y;
 		loot->TRANS->SetPosition(this->GetPosition() + Vector3(0, yoffset, 0));
 		loot->RIGID->AddForce({ 0,50,0 });
 		loot->RIGID->SetVel({ 0,75,0 });
-
+		loot->AddComponent(new ParticleSpawnerScript(DataContainer::GetInstance()->GetGameObjectRaw("particlebullettrail"), 30, Vector3(), 0));
+		
 
 		return;
 	}
--- Physics/Source/SpawnLootScript.h
+++ Physics/Source/SpawnLootScript.h
@@ -3,14 +3,19 @@
 
 /*
 author: lorenzo yanga
-desc: just an identifier for now to discriminate in the observer
+desc: just an identifier for now to discriminate in the observer, also a public boolean that 
 */
 
 class SpawnLootScript : public ScriptComponent
 {
+private:
 public:
+
+	bool m_isActivated;
+
 	SpawnLootScript();
 	~SpawnLootScript();
 
 	virtual Component* Clone() { return new SpawnLootScript(*this); }
+
 };
